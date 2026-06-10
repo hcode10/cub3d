5884fde3aa7f15c4dc37888f42be26fb543521bf
hcode10 <hcode10@gmail.com>
2026-06-10 14:35:46 +0200

    Feur

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Arguments incorrect : ./cub3d /path/to/map.cub");
+        return (1);
+    }
+
+    
+    
+}
---
+++ src/parsing/parsing.c
@@ -0,0 +1,6 @@
+#include "../includes/parsing.h"
+
+bool parsing(char *map_path)
+{
+
+}
