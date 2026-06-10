cc6d55ff7f7c4026b7d4932ed51016fe5ba23a94
hcode10 <hcode10@gmail.com>
2026-06-10 10:20:58 +0200

    Ajout structure initiale pour le parsing

---
+++ includes/parsing.h
@@ -0,0 +1,23 @@
+#ifndef CUB_PARSE
+# define CUB_PARSE
+
+typedef struct s_player_pos
+{
+	double	x;
+	double	y;
+}	t_player_pos;
+
+typedef struct s_map
+{
+	char		**map;
+	char		**map_dup;
+	int		    floor_color[3];
+	int		    sky_color[3];
+	char		*texture_no;
+	char		*texture_so;
+	char		*texture_we;
+	char		*texture_ea;
+	t_player_pos	pos;
+}	t_map;
+
+#endif
