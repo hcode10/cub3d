2ebd9478ad6251e5134ca2dbbd3f7bf3c2543c26
dcasadio <dcasadio@f1r9s12.paris.42.school>
2026-06-11 18:09:54 +0200

    Ajout des fonction set et check

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,7 +6,11 @@
 /*   By: dcasadio <dcasadio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 10:12:10 by coressor          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/06/12 12:21:31 by dcasadio         ###   ########.fr       */
+=======
+/*   Updated: 2026/06/11 16:16:55 by dcasadio         ###   ########.fr       */
+>>>>>>> b80446b (Ajout des fonction set et check)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +19,14 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
+<<<<<<< HEAD
+=======
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+>>>>>>> b80446b (Ajout des fonction set et check)
 
 #endif
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,57 +6,114 @@
 /*   By: dcasadio <dcasadio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 16:11:55 by dcasadio          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/06/21 16:34:13 by coressor         ###   ########.fr       */
+=======
+/*   Updated: 2026/06/11 18:07:40 by dcasadio         ###   ########.fr       */
+>>>>>>> b80446b (Ajout des fonction set et check)
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
-bool	valid_path(char *map_path)
+static bool	valid_path(char *map_path)
 {
-	(void)map_path;
+	char	*ext;
 
+	ext = map_path + (ft_strlen(map_path) - 4);
+	printf("ext = %s\n", ext);
+	if (ft_strncmp(ext, ".cub", 4) != 0)
+	{
+		ft_putstr_fd("Erreur : Veuillez verifier le chemin de map fourni !", 1);
+		return (false);
+	}
 	return (true);
 }
 
-bool	parsing(char *map_path, t_map *map)
+static bool	check_textures(t_map *map)
 {
-	int		fd;
+	//changer pour open access interdit
+	if (!access(map->texture_ea, F_OK) == 0)
+		return (false);
+	if (!access(map->texture_no, F_OK) == 0)
+		return (false);
+	if (!access(map->texture_so, F_OK) == 0)
+		return (false);
+	if (!access(map->texture_we, F_OK) == 0)
+		return (false);
+	return (true);
+}
+
+static bool	check_color(int *sky_color, int *floor_color)
+{
+	int	i;
+
+	i = 0;
+	while (i < 3)
+	{
+		if (sky_color[i] >= 0 && sky_color[i] <= 255)
+			i++;
+		else
+			return (false);
+	}
+	i = 0;
+	while (i < 3)
+	{
+		if (floor_color[i] >= 0 && floor_color[i] <= 255)
+			i++;
+		else
+			return (false);
+	}
+	return (true);
+}
+
+bool	set_textures(t_map *map, char *map_path)
+{
 	char	*line;
 	char	**splited;
-	char	**color_split;
+	int		fd;
 
-	if (!valid_path(map_path))
-		return (false);
 	fd = open(map_path, O_RDONLY);
+	printf("fd = %d\n", fd);
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		splited = ft_split(line, ' ');
 		if (ft_strncmp("NO", splited[0], 2) == 0)
-		{
 			map->texture_no = ft_strtrim(splited[1], "\n");
-		}
 		else if (ft_strncmp("SO", splited[0], 2) == 0)
-		{
 			map->texture_so = ft_strtrim(splited[1], "\n");
-		}
 		else if (ft_strncmp("WE", splited[0], 2) == 0)
-		{
 			map->texture_we = ft_strtrim(splited[1], "\n");
-		}
 		else if (ft_strncmp("EA", splited[0], 2) == 0)
-		{
 			map->texture_ea = ft_strtrim(splited[1], "\n");
-		}
-		else if (ft_strncmp("F", splited[0], 1) == 0)
+		free(splited);
+	}
+	close(fd);
+	return (false);
+}
+
+bool	set_color(t_map *map, char *map_path)
+{
+	char	*line;
+	char	**splited;
+	char	**color_split;
+	int		fd;
+
+	fd = open(map_path, O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		splited = ft_split(line, ' ');
+		if (ft_strncmp("F", splited[0], 1) == 0)
 		{
 			color_split = ft_split(splited[1], ',');
 			map->floor_color[0] = ft_atoi(color_split[0]);
 			map->floor_color[1] = ft_atoi(color_split[1]);
 			map->floor_color[2] = ft_atoi(color_split[2]);
 		}
-		else if (ft_strncmp("C", splited[0], 2) == 0)
+		else if (ft_strncmp("C", splited[0], 1) == 0)
 		{
 			color_split = ft_split(splited[1], ',');
 			map->sky_color[0] = ft_atoi(color_split[0]);
@@ -65,10 +122,21 @@ bool	parsing(char *map_path, t_map *map)
 		}
 		free(splited);
 	}
+	close(fd);
+	return (false);
+}
 
-	printf("texture = \n{%s$ %s$ %s$ %s$};", map->texture_ea, map->texture_no, map->texture_so, map->texture_we);
-	printf("texture = \n{%s$ %s$ %s$ %s$};", map->texture_ea, map->texture_no, map->texture_so, map->texture_we);
-	
-	(void)map;
+bool	parsing(char *map_path, t_map *map)
+{
+	if (!valid_path(map_path))
+		return (false);
+	set_textures(map, map_path);
+	set_color(map, map_path);
+
+	if (!check_color(map->sky_color, map->floor_color))
+		return (perror("RGB : Valeurs hors plage (0, 255)"), false);
+
+	if (!check_textures(map))
+		return (perror("Veuillez verifier le chemin des textures !"), false);
 	return (true);
 }
