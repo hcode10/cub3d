5c697b5a822279e54263ac94f7a473515b97f169
dcasadio <dcasadio@f1r9s12.paris.42.school>
2026-06-11 16:05:17 +0200

    Debut parsing

--- src/main.c
+++ src/main.c
@@ -5,12 +5,15 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        printf("Arguments incorrect : ./cub3d /path/to/map.cub\n");
-        return (1);
-    }
-    if (!parsing(argv[1]))
-        return (1);
-    return (0);
+	t_map *map;
+	if (argc != 2)
+	{
+		printf("Arguments incorrect : ./cub3d /path/to/map.cub\n");
+		return (1);
+	}
+	map = malloc(sizeof(t_map));
+	if (!map)
+		return (1);
+	parsing(argv[1], map);
+	return (0);
 }
