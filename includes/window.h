52a53ef44903093e6a96b7802caa3cffd9f702f2
coressor <coressor@f1r10s14.paris.42.school>
2026-06-11 16:15:07 +0200

    fix: makefile fix

--- includes/window.h
+++ includes/window.h
@@ -6,12 +6,17 @@
 /*   By: coressor <coressor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 14:41:56 by coressor          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/06/10 17:55:55 by coressor         ###   ########.fr       */
+=======
+/*   Updated: 2026/06/10 14:48:01 by coressor         ###   ########.fr       */
+>>>>>>> 073a904 (fix: makefile fix)
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINDOW_H
 # define WINDOW_H
+<<<<<<< HEAD
 # include "../minilibx-linux/mlx.h"
 # include <unistd.h>
 # include <stdlib.h>
@@ -29,3 +34,14 @@ void	*init_window();
 void	free_window(t_window *s_win);
 
 #endif
+=======
+
+typedef struct s_window
+{
+	
+}
+
+#endif
+
+
+>>>>>>> 073a904 (fix: makefile fix)
