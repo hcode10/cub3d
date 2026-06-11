a818119dffd7480ec1d6e29c8d90184f967f1705
dcasadio <dcasadio@f1r9s12.paris.42.school>
2026-06-11 16:05:17 +0200

    Debut parsing

--- includes/parsing.h
+++ includes/parsing.h
@@ -1,7 +1,11 @@
 #ifndef CUB_PARSE
 # define CUB_PARSE
 
-#include <stdbool.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include "../libft/libft.h"
 
 typedef struct s_player_pos
 {
@@ -11,17 +15,17 @@ typedef struct s_player_pos
 
 typedef struct s_map
 {
-	char		**map;
-	char		**map_dup;
-	int		    floor_color[3];
-	int		    sky_color[3];
-	char		*texture_no;
-	char		*texture_so;
-	char		*texture_we;
-	char		*texture_ea;
+	char			**map;
+	char			**map_dup;
+	int				floor_color[3];
+	int				sky_color[3];
+	char			*texture_no;
+	char			*texture_so;
+	char			*texture_we;
+	char			*texture_ea;
 	t_player_pos	pos;
 }	t_map;
 
-bool	parsing(char *map_path);
+bool	parsing(char *map_path, t_map *map);
 
 #endif
