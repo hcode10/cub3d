a116867a45778e0e2978931674b5a868c3e9fdbc
dcasadio <dcasadio@f1r10s14.paris.42.school>
2026-06-12 16:55:11 +0200

    Parsing de la map quasi fini

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -3,107 +3,131 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coressor <coressor@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dcasadio <dcasadio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 10:11:11 by coressor          #+#    #+#             */
-/*   Updated: 2026/02/18 14:58:27 by coressor         ###   ########.fr       */
+/*   Updated: 2026/06/12 12:29:30 by dcasadio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/parsing.h"
 #include "get_next_line.h"
 
-static char	*read_n_save(int fd, char *str)
+static char	*get_line(char *storage)
 {
-	char	*buff;
-	int		read_ret;
-	char	*s;
-
-	s = NULL;
-	buff = malloc(BUFFER_SIZE * sizeof(char) + 1);
-	if (!buff)
-		return (free(str), NULL);
-	read_ret = 1;
-	while (!ft_strchr(str, '\n') && read_ret != 0)
-	{
-		read_ret = read(fd, buff, BUFFER_SIZE);
-		if (read_ret == -1)
-			return (free(str), free(buff), NULL);
-		buff[read_ret] = 0;
-		str = ft_strjoin(str, buff);
-		if (!str)
-			return (free(buff), NULL);
-	}
-	free(s);
-	free(buff);
-	return (str);
-}
-
-static char	*extract_line(char *str)
-{
+	int		i;
 	char	*line;
-	size_t	i;
-	size_t	len;
 
-	i = 0;
-	len = 0;
-	if (!str || !str[0])
+	if (!storage || !storage[0])
 		return (NULL);
-	while (str[len] != '\n' && str[len])
-		len++;
-	if (str[len] == '\n')
-		len++;
-	line = malloc(len * sizeof(char) + 1);
+	i = 0;
+	while (storage[i] && storage[i] != '\n')
+		i++;
+	line = malloc(i + (storage[i] == '\n') + 1);
 	if (!line)
 		return (NULL);
-	while (i < len)
+	i = 0;
+	while (storage[i] && storage[i] != '\n')
 	{
-		line[i] = str[i];
+		line[i] = storage[i];
 		i++;
 	}
-	line[i] = 0;
+	if (storage[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
 	return (line);
 }
 
-static char	*update_str(char *str)
+static char	*read_and_join(char *storage, int fd, int *eof, int *found_nl)
 {
-	char	*new_str;
+	char	*buffer;
+	char	*temp;
+	int		bytes_read;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (free(storage), NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
+		return (free(buffer), free(storage), NULL);
+	if (bytes_read == 0)
+		return (*eof = 1, free(buffer), storage);
+	buffer[bytes_read] = '\0';
+	if (ft_strchr(buffer, '\n'))
+		*found_nl = 1;
+	temp = ft_strjoin(storage, buffer);
+	free(buffer);
+	if (!temp)
+		return (free(storage), NULL);
+	free(storage);
+	return (temp);
+}
+
+static char	*read_more(char *storage, int fd)
+{
+	char	*temp;
+	int		eof_reached;
+	int		found_nl;
+
+	eof_reached = 0;
+	found_nl = 0;
+	if (!storage)
+	{
+		storage = malloc(1);
+		if (!storage)
+			return (NULL);
+		storage[0] = '\0';
+	}
+	while (!eof_reached && !found_nl)
+	{
+		temp = read_and_join(storage, fd, &eof_reached, &found_nl);
+		if (!temp)
+			return (NULL);
+		storage = temp;
+		if (storage[0] == '\0')
+			return (storage);
+	}
+	return (storage);
+}
+
+static char	*update_storage(char *storage)
+{
 	size_t	i;
 	size_t	j;
+	char	*new_storage;
 
-	j = 0;
 	i = 0;
-	while (str[j] && str[j] != '\n')
-		j++;
-	if (!str[j])
-		return (free(str), NULL);
-	new_str = malloc((ft_strlen(str) - j) * sizeof(char));
-	if (!new_str)
-		return (free(str), NULL);
-	j++;
-	while (str[j])
-	{
-		new_str[i] = str[j];
+	while (storage[i] && storage[i] != '\n')
 		i++;
-		j++;
-	}
-	new_str[i] = 0;
-	free(str);
-	return (new_str);
+	if (!storage[i] || !storage[i + 1])
+		return (free(storage), NULL);
+	i++;
+	new_storage = malloc(ft_strlen(storage + i) + 1);
+	if (!new_storage)
+		return (free(storage), NULL);
+	j = 0;
+	while (storage[i])
+		new_storage[j++] = storage[i++];
+	new_storage[j] = '\0';
+	free(storage);
+	return (new_storage);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*str;
+	static char	*storage;
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	str = read_n_save(fd, str);
-	if (!str)
+	storage = read_more(storage, fd);
+	if (!storage)
 		return (NULL);
-	line = extract_line(str);
+	if (!storage[0])
+		return (free(storage), storage = NULL, NULL);
+	line = get_line(storage);
 	if (!line)
-		return (free(str), str = NULL, NULL);
-	str = update_str(str);
+		return (free(storage), storage = NULL, NULL);
+	storage = update_storage(storage);
 	return (line);
 }
--- src/main.c
+++ src/main.c
@@ -5,16 +5,13 @@
 
 int main(int argc, char **argv)
 {
-	t_map	*map;
+	t_map	map;
 
 	if (argc != 2)
 	{
 		printf("Arguments incorrect : ./cub3d /path/to/map.cub\n");
 		return (1);
 	}
-	map = malloc(sizeof(t_map));
-	if (!map)
-		return (1);
-	parsing(argv[1], map);
+	parsing(argv[1], &map);
 	return (0);
 }
