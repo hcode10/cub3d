ddcb1a0f737c475cc16d489de9a9f22270f25da9
hcode10 <hcode10@gmail.com>
2026-06-20 22:10:34 +0200

    Putain de git de merde

--- includes/parsing.h
+++ includes/parsing.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -43,3 +44,50 @@ void 	free_tabs(char **tab);
 void	free_struct(t_map *map);
 
 #endif
+=======
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcasadio <dcasadio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/11 16:11:51 by dcasadio          #+#    #+#             */
+/*   Updated: 2026/06/11 16:11:52 by dcasadio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_PARSE
+# define CUB_PARSE
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include "../libft/libft.h"
+
+typedef struct s_player_pos
+{
+	double	x;
+	double	y;
+}	t_player_pos;
+
+typedef struct s_map
+{
+	char			**map;
+	char			**map_dup;
+	int				floor_color[3];
+	int				sky_color[3];
+	char			*texture_no;
+	char			*texture_so;
+	char			*texture_we;
+	char			*texture_ea;
+	t_player_pos	pos;
+}	t_map;
+
+bool	parsing(char *map_path, t_map *map);
+void 	free_tabs(char **tab);
+void	free_struct(t_map *map);
+
+#endif
+>>>>>>> 80b9be9 (Putain de git de merde)
