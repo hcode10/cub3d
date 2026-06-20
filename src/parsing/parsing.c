5b3d607dd08bf6bfaf760a2091510cc1b3cdee9e
hcode10 <hcode10@gmail.com>
2026-06-20 21:14:27 +0200

    Feat : Parsing

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -15,6 +15,42 @@
 #include <string.h>
 #include <errno.h>
 
+size_t count_tabs(char **tab)
+{
+	size_t count;
+
+	count = 0;
+	while (tab[count])
+		count++;
+	return (count);
+}
+
+void free_tabs(char **tab)
+{
+	size_t i;
+
+	i = 0;
+	while (tab[i])
+		free(tab[i++]);
+	free(tab);
+}
+
+void free_struct(t_map *map)
+{
+	if (map->map)
+		free_split(map->map);
+	if (map->map_dup)
+		free(map->map_dup);
+	if (map->texture_ea)
+		free(map->texture_ea);
+	if (map->texture_no)
+		free(map->texture_no);
+	if (map->texture_so)
+		free(map->texture_so);
+	if (map->texture_we)
+		free(map->texture_we);
+}
+
 static bool	valid_path(char *map_path)
 {
 	char	*ext;
@@ -22,7 +58,9 @@ static bool	valid_path(char *map_path)
 	ext = map_path + (ft_strlen(map_path) - 4);
 	printf("ext = %s\n", ext);
 	if (ft_strncmp(ext, ".cub", 4) != 0)
-		return (perror("Veuillez verifier le chemin de map fourni !"), false);
+	{
+		return (false);
+	}
 	return (true);
 }
 
@@ -30,6 +68,8 @@ static bool check_files(char *path)
 {
 	int fd;
 
+	if (!path)
+		return (false);
 	fd = open(path, O_RDONLY);
 	if (fd <= 0)
 		return (false);
@@ -83,11 +123,19 @@ bool	set_textures(t_map *map, char *map_path)
 	int		fd;
 
 	fd = open(map_path, O_RDONLY);
-	if (fd <= 0)
+	if (fd < 0)
 		return (false);
 	while ((line = get_next_line(fd)) != NULL)
 	{
 		splited = ft_split(line, ' ');
+		free(line);
+		if (!splited)
+			continue ;
+		if (count_tabs(splited) != 2)
+		{
+			free_tabs(splited);
+			return (false);
+		}
 		if (ft_strncmp("NO", splited[0], 2) == 0)
 			map->texture_no = ft_strtrim(splited[1], "\n");
 		else if (ft_strncmp("SO", splited[0], 2) == 0)
@@ -96,10 +144,10 @@ bool	set_textures(t_map *map, char *map_path)
 			map->texture_we = ft_strtrim(splited[1], "\n");
 		else if (ft_strncmp("EA", splited[0], 2) == 0)
 			map->texture_ea = ft_strtrim(splited[1], "\n");
-		free(splited);
+		free_tabs(splited);
 	}
 	close(fd);
-	return (false);
+	return (true);
 }
 
 bool	set_color(t_map *map, char *map_path)
@@ -122,23 +170,30 @@ bool	set_color(t_map *map, char *map_path)
 			color_split = ft_split(splited[1], ',');
 			if (!color_split)
 				return (free(splited), false);
+			if (count_tabs(color_split) != 3)
+				return (free_tabs(color_split), free_tabs(splited), free(line), false);
 			map->floor_color[0] = ft_atoi(color_split[0]);
 			map->floor_color[1] = ft_atoi(color_split[1]);
 			map->floor_color[2] = ft_atoi(color_split[2]);
+			free_tabs(color_split);
 		}
 		else if (ft_strncmp("C", splited[0], 1) == 0)
 		{
 			color_split = ft_split(splited[1], ',');
 			if (!color_split)
-				return (free(splited), false);
+				return (free_tabs(splited), false);
+			if (count_tabs(color_split) != 3)
+				return (free_tabs(color_split), free_tabs(splited), free(line), false);
 			map->sky_color[0] = ft_atoi(color_split[0]);
 			map->sky_color[1] = ft_atoi(color_split[1]);
 			map->sky_color[2] = ft_atoi(color_split[2]);
+			free_tabs(color_split);
 		}
-		free(splited);
+		free_tabs(splited);
+		free(line);
 	}
 	close(fd);
-	return (false);
+	return (true);
 }
 
 bool	space_only(char *str)
@@ -161,22 +216,54 @@ bool	space_only(char *str)
 	return (true);
 }
 
+bool space_is_ugly(t_map *map)
+{
+	size_t index = 0;
+	while (map->map[index])
+	{
+		printf("%s", map->map[index]);
+		index++;
+	}
+	return (true);
+}
+
+static bool	add_map_line(t_map *map, char *line)
+{
+	size_t	n;
+	size_t	i;
+	char	**new;
+
+	n = 0;
+	while (map->map && map->map[n])
+		n++;
+	new = ft_calloc(n + 2, sizeof(char *));
+	if (!new)
+		return (false);
+	i = 0;
+	while (i < n)
+	{
+		new[i] = map->map[i];
+		i++;
+	}
+	new[n] = ft_strtrim(line, "\n");
+	if (!new[n])
+		return (free(new), false);
+	free(map->map);
+	map->map = new;
+	return (true);
+}
+
 bool	get_map(char *map_path, t_map *map)
 {
 	int		fd;
 	char	*line;
 	int		line_ok;
 	char	**splited;
-	int		index;
 
 	fd = open(map_path, O_RDONLY);
 	if (fd <= 0)
 		return (false);
-	line = ft_calloc(1, 2);
-	if (!line)
-		return (NULL);
-	line[0] = ' ';
-	line[1] = '\0';
+	line_ok = 0;
 	while (1)
 	{
 		line = get_next_line(fd);
@@ -195,42 +282,73 @@ bool	get_map(char *map_path, t_map *map)
 			line_ok++;
 		else if (ft_strncmp("C", splited[0], 1) == 0)
 			line_ok++;
-		free(splited);
+		free_tabs(splited);
+		free(line);
 	}
 	if (line_ok > 6 || line_ok < 6)
 		return (false);
-	while (space_only(line))
+	while (line && space_only(line))
+	{
+		free(line);
 		line = get_next_line(fd);
-	index = 0;
+	}
+	if (!line)
+		return (false);
+	map->map = NULL;
 	while (line != NULL)
 	{
-		printf("$%s$\n", line);
-		map->map = ft_calloc(1, ft_strlen(line) + 1);
-		if (!map->map[index])
-			return (NULL);
+		if (!add_map_line(map, line))
+		{
+			free(line);
+			close(fd);
+			return (false);
+		}
+		free(line);
 		line = get_next_line(fd);
 	}
 	close(fd);
+	space_is_ugly(map);
 	return (true);
 }
 
 bool	parsing(char *map_path, t_map *map)
 {
+	map->map_dup = ft_calloc(1, 1);
 	if (!valid_path(map_path))
+	{
+		printf("Veuillez verifier le chemin de map fourni !\n");
 		return (false);
+	}
 	printf("valid_path OK !\n");
-	set_textures(map, map_path);
+
+	if (!set_textures(map, map_path))
+	{
+		printf("RGB : Textures invalides\n");
+	}
 	printf("set_textures OK !\n");
-	set_color(map, map_path);
+
+	if (!set_color(map, map_path))
+	{
+		printf("RGB : Valeurs invalides\n");
+		return (false);
+	}
+
 	printf("set_color OK !\n");
 	if (!check_color(map->sky_color, map->floor_color))
-		return (perror("RGB : Valeurs hors plage (0, 255)"), false);
+	{
+		printf("RGB : Valeurs hors plage (0, 255)\n");
+		return (false);
+	}
 	printf("check_color OK !\n");
 	if (!check_textures(map))
 		return (perror("Veuillez verifier le chemin des textures !"), false);
 	printf("check_textures OK !\n");
 
-	get_map(map_path, map);
+	if (!get_map(map_path, map))
+	{
+		printf("Impossible d'afficher la map !\n");
+		return (false);
+	}
 	int line = 0;
 	while (map->map[line])
 	{
