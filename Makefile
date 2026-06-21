d66b827bc5b3ed8fdb065a0aebc115c06e1c1788
dcasadio <dcasadio@f1r8s11.paris.42.school>
2026-06-21 12:02:23 +0200

    [*ta de amdre

--- Makefile
+++ Makefile
@@ -43,10 +43,16 @@ SRC_GAME = $(DIR_GAME)/game.c \
 
 #-------- ALL THE SOURCES -----
 SRC = $(SRC_DIR)/main.c \
-      $(SRC_PARSING)/parsing.c \
         $(SRC_UTILS) \
         $(SRC_WINDOW) \
 		$(SRC_GAME) \
+	  $(SRC_PARSING)/parsing.c \
+	  $(SRC_PARSING)/map_parse.c \
+	  $(SRC_PARSING)/map_utils.c \
+	  $(SRC_PARSING)/map_validate.c \
+	  $(SRC_PARSING)/parsing_utils.c \
+	  $(SRC_PARSING)/flood_fill.c \
+		# $(SRC_WINDOW) \
 
 MAPS = ./maps
 OBJS = $(SRC:.c=.o)
--- src/main.c
+++ src/main.c
@@ -12,23 +12,41 @@
 
 #include "parsing.h"
 
+/*static int	validate_map(t_map *game)
+{
+	if (!validate_map_chars(game))
+	{
+		return (-1);
+	}
+	if (is_map_solvable(game) == -1)
+		return (-1);
+	return (0);
+}*/
+
+static bool	check_before_run(t_map *game)
+{
+	validate_map_chars(game);
+	return (true);
+}
+
 int main(int argc, char **argv)
 {
-	t_map	map;
+	t_map	*map;
 
 	ft_bzero(&map, sizeof(t_map));
+	map = ft_calloc(1, sizeof(t_map));
 	if (argc != 2)
 	{
 		printf("Arguments incorrect : ./cub3d /path/to/map.cub\n");
 		return (1);
 	}
-	if (!parsing(argv[1], &map))
+	parsing(argv[1], map);
+
+	if (check_before_run(map))
 	{
-		printf("Erreur de parsing !\n");
-		free_struct(&map);
-		return (1);
+		printf("Flood ok !\n");
+		printf("Player position X = %f, Y = %f\n", map->player_x, map->player_y);
+		is_map_solvable(map);
 	}
-	printf("Parsing OK");
-	free_struct(&map);
 	return (0);
 }
