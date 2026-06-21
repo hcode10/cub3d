a98069d9dff7543b59e31bce3a11c073d8fd508a
dcasadio <dcasadio@f1r8s11.paris.42.school>
2026-06-21 10:43:58 +0200

    Git e merde

---
+++ src/parsing/map_utils.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcasadio <dcasadio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 16:01:12 by dcasadio          #+#    #+#             */
+/*   Updated: 2026/06/14 20:46:28 by dcasadio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+#include "../libft/get_next_line.h"
+#include "../includes/parsing.h"
+
+
+void	free_map(char **map)
+{
+	int	i;
+
+	if (!map)
+		return ;
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
+
+char	**copy_map(char **map)
+{
+	int		i;
+	int		map_line;
+	char	**map_copy;
+
+	map_line = 0;
+	while (map[map_line])
+		map_line++;
+	map_copy = malloc(sizeof(char *) * (map_line + 1));
+	if (!map_copy)
+		return (NULL);
+	i = 0;
+	while (i <= map_line)
+		map_copy[i++] = NULL;
+	map_line = 0;
+	while (map[map_line])
+	{
+		map_copy[map_line] = ft_strdup(map[map_line]);
+		if (!map_copy[map_line])
+			return (free_map(map_copy), NULL);
+		map_line++;
+	}
+	map_copy[map_line] = NULL;
+	return (map_copy);
+}
+
+void	aff_map(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			ft_putchar_fd(map[i][j], 1);
+			j++;
+		}
+		i++;
+		ft_putchar_fd('\n', 1);
+	}
+	ft_putchar_fd('\n', 1);
+}
