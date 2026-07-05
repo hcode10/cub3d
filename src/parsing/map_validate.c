dee3eda374f530b8362ef26c9ca711909bcb37f6
coressor <coressor@f2r7s1.paris.42.school>
2026-07-05 19:22:49 +0200

    add: finish draw

--- src/parsing/map_validate.c
+++ src/parsing/map_validate.c
@@ -6,34 +6,30 @@
 /*   By: dcasadio <dcasadio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 16:01:12 by dcasadio          #+#    #+#             */
-/*   Updated: 2026/07/05 19:17:59 by coressor         ###   ########.fr       */
+/*   Updated: 2026/07/05 19:21:26 by coressor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
-#include "../libft/get_next_line.h"
-#include "../includes/parsing.h"
+#include "parsing.h"
 
 static void	check_player_collect(t_map *game, int i, int j)
 {
-	int	pos;
+	char	pos;
 
 	pos = game->map[i][j];
 	if (pos == 'S' || pos == 'N' || pos == 'W' || pos == 'E')
 	{
-		game->p_pos.y = (double)i + 0.5;
-		game->p_pos.x = (double)j + 0.5;
+		game->p_pos.y = (double)i;
+		game->p_pos.x = (double)j;
 	}
 }
 
-int	validate_map_chars(t_map *game)
+bool	validate_map_chars(t_map *game)
 {
 	int		i;
 	int		j;
-	char	*valid_chars;
 
 	i = 0;
-	valid_chars = "10NSEW \n";
 	game->p_pos.x = 0;
 	game->p_pos.y = 0;
 	while (game->map[i] != NULL)
@@ -41,43 +37,16 @@ int	validate_map_chars(t_map *game)
 		j = 0;
 		while (game->map[i][j])
 		{
-			//printf("i : %d j = %d carac = +%c+\n", i, j, game->map[i][j]);
-			if (!ft_strchr(valid_chars, game->map[i][j]))
-			{
-				return (-1);
-			}
+			if (!ft_strchr("10NSEW \n", game->map[i][j]))
+				return (dbg_val("char invalide", game->map[i]),
+					error_msg("Map: caractere invalide"), false);
 			check_player_collect(game, i, j);
 			j++;
 		}
 		i++;
 	}
 	if (game->p_pos.x == 0 || game->p_pos.y == 0)
-		return (-1);
-	return (0);
-}
-
-int	validate_map_walls(t_map *game)
-{
-	size_t	i;
-	size_t	last_line;
-
-	last_line = 0;
-	while (game->map[last_line])
-		last_line++;
-	last_line--;
-	i = 0;
-	while (i < game->width)
-	{
-		if (game->map[0][i] != '1' || game->map[last_line][i] != '1')
-			return (-1);
-		i++;
-	}
-	i = 0;
-	while (game->map[i])
-	{
-		if (game->map[i][0] != '1' || game->map[i][game->width - 1] != '1')
-			return (-1);
-		i++;
-	}
-	return (0);
-}
+		return (dbg_fail("validate", "joueur absent ou en bord (x/y==0)"),
+			error_msg("Map: joueur absent ou mal place"), false);
+	return (true);
+} 
